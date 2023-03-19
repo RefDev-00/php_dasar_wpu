737d44ef8f6e08abd6acba281f44e3fb75d73149
Refsi Maulana <refsimaulanatkr@gmail.com>
2023-03-19 22:18:05 +0700

    php+mysql

---
+++ php+mysql/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Website Mahasiswa</title>
+</head>
+
+<body>
+  <h1>Halaman Data Mahasiswa</h1>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>No</th>
+      <th>Nama</th>
+      <th>NRP</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Gambar</th>
+      <th>Aksi</th>
+    </tr>
+  </table>
+</body>
+
+</html>
